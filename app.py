ad1db6fca33aef0720e61779ec252f249f2f12c5
Mansi Maurya <mansimaurya0@gmail.com>
2021-04-20 20:57:47 +0530

    Images rendered through voice

--- app.py
+++ app.py
@@ -4,13 +4,14 @@ from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
 from functools import wraps
 from idiot import search
+from  search_cosine_similarity import cosine_similarity_T
 # from asr_myntra import speech_text
 
 
 def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
-    return conn
+    return conn 
 
 
 app = Flask(__name__)
@@ -21,30 +22,36 @@ def index():
 
 
 
+# @app.route('/voice',methods=['POST'])
+# def voice():
+#     query = request.form['voice_query'].lower()
+#     tokens = query.split()
+#     print(tokens)
+#     if "north" in tokens:
+#         if "east" in tokens or "eastern" in tokens:
+#             return redirect('http://localhost:5000/region/north-east')
+#         else:
+#             return redirect('http://localhost:5000/region/north')
+#     elif "west" in tokens or "western" in tokens:
+#         return redirect('http://localhost:5000/region/western')
+#     elif "south" in tokens or "southern" in tokens:
+#         return redirect('http://localhost:5000/region/south') 
+#     elif "east" in tokens or "eastern" in tokens:
+#         return redirect('http://localhost:5000/region/east')
+#     elif "northeastern" in tokens:
+#         return redirect('http://localhost:5000/region/north-east')
+    
+#     return render_template("home.html")
+
+
 @app.route('/voice',methods=['POST'])
 def voice():
     query = request.form['voice_query'].lower()
-    tokens = query.split()
-    print(tokens)
-    if "north" in tokens:
-        if "east" in tokens or "eastern" in tokens:
-            return redirect('http://localhost:5000/region/north-east')
-        else:
-            return redirect('http://localhost:5000/region/north')
-    elif "west" in tokens or "western" in tokens:
-        return redirect('http://localhost:5000/region/western')
-    elif "south" in tokens or "southern" in tokens:
-        return redirect('http://localhost:5000/region/south') 
-    elif "east" in tokens or "eastern" in tokens:
-        return redirect('http://localhost:5000/region/east')
-    elif "northeastern" in tokens:
-        return redirect('http://localhost:5000/region/north-east')
-
-
-
-
+    print(query)
+    dataframe = cosine_similarity_T(10,query)
+    print(dataframe)
+    return render_template("results.html",data=dataframe)
     
-    return render_template("home.html")
    
 
 
